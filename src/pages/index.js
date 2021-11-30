50b5ae2d3ccbd525d2d20c5250fe4c6d6c3370bd
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2021-11-30 14:53:56 +0000

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import "../styles/styles.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="wrapper">
+      <nav className="nav">nav</nav>
+    </div>
+  )
 }
